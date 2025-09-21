349126444919e0face3d2ef9b0e591044ace3db4
Gustavo Henrique <ogustavohp@gmail.com>
2025-09-21 18:23:08 -0300

    create link's

--- src/renderer/routes.tsx
+++ src/renderer/routes.tsx
@@ -2,14 +2,17 @@ import { Router } from '../lib/electron-router-dom'
 import { Route } from 'react-router-dom'
 import { Blank } from './src/pages/blank'
 import { Document } from './src/pages/document'
+import { DefaultLayout } from './src/pages/layouts/default'
 
 export function Routes() {
   return (
     <Router
       main={
         <>
-          <Route path="/" element={<Blank />} />
-          <Route path="/documents" element={<Document />} />
+          <Route path='/' element={<DefaultLayout />} >
+            <Route path="/" element={<Blank />} />
+            <Route path="/documents" element={<Document />} />
+          </Route>
         </>
       }
     />
--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,15 +1,7 @@
 import { Routes } from "../routes";
-import { Header } from "./components/Header";
-import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
-    <div className="h-screen w-screen text-notion-100 flex">
-      <Sidebar />
-      <div className="flex-1 flex flex-col max-h-screen">
-        <Header />
-        <Routes />
-      </div>
-    </div>
+    <Routes />
   )
 }
