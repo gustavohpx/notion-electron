981eb8204dc95af53df81ae8bb139a289e2456f9
Gustavo Henrique <ogustavohp@gmail.com>
2025-09-21 01:46:03 -0300

    create componentes and configure drag region

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,16 @@
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
 export function App() {
   return (
-    <div className="h-screen w-screen text-notion-100">a</div>
+    <div className="h-screen w-screen text-notion-100 flex">
+      <Sidebar />
+      <div className="flex-1 flex flex-col max-h-screen">
+        <Header />
+        <main className="flex-1 flex items-center justify-center text-notion-400">
+          Select or create a document
+        </main>
+      </div>
+    </div>
   )
 }
