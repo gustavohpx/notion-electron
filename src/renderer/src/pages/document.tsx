1c5026f6ea97231c17d477163bc6281181c0c771
Gustavo Henrique <ogustavohp@gmail.com>
2025-09-21 19:01:46 -0300

    add tiptap and create document page

--- src/renderer/src/pages/document.tsx
+++ src/renderer/src/pages/document.tsx
@@ -1,12 +1,27 @@
 import { Link } from "react-router-dom";
+import { ToC } from "../components/ToC";
 
 export function Document() {
   return (
-    <main className="flex-1 flex items-center justify-center text-notion-400">
-      Document
-      <Link to="/" className="text-notion-200 ml-2">
-        Go to Home Page
-      </Link>
+    <main className="flex-1 flex py-12 px-10 gap-8">
+      <aside className="hidden lg:block sticky top-0">
+        <span className="text-notion-300 font-semibold text-xs uppercase">
+          table of contents
+        </span>
+
+        <ToC.Root>
+          <ToC.Link>Back-end</ToC.Link>
+          <ToC.Section>
+            <ToC.Link>Node.js</ToC.Link>
+            <ToC.Link>Deno</ToC.Link>
+            <ToC.Link>Ruby on Rails</ToC.Link>
+          </ToC.Section>
+        </ToC.Root>
+      </aside>
+
+      <section className="flex-1 flex flex-col items-center">
+        hhhhyfytjkkyg
+      </section>
     </main>
   )
 }
